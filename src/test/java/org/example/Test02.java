aec10c7ae2fae710b266a1d79bdbd432d8df9b8f
yangjh <3103425802@qq.com>
2021-08-17 15:39:53 +0800

    c2客户

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -3,5 +3,6 @@ package org.example;
 public class Test02  {
     public static void main(String[] args) {
         System.out.println("test02....");
+        System.out.println("c2客户端->test02...");
     }
 }
